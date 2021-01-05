2ecd23ebada58745303fa61201b22c61cd97350a
hector4213 <hectorpwns@gmail.com>
2021-01-05 09:45:51 -0500

    reverted

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_114505) do
+ActiveRecord::Schema.define(version: 2021_01_05_031900) do
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.string "user"
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -27,11 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_114505) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
 end
