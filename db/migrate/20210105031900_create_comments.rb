1497cd13384369b066a325e94405f49a774c20e5
hector4213 <hectorpwns@gmail.com>
2021-01-05 08:28:35 -0500

    revert table

--- db/migrate/20210105031900_create_comments.rb
+++ db/migrate/20210105031900_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
+      t.string :user
       t.text :body
       t.references :post, null: false, foreign_key: true
 
