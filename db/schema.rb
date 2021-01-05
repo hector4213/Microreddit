b3c5625f9df7649d24f438fdda2559708b78e0ff
hector4213 <hectorpwns@gmail.com>
2021-01-05 06:35:59 -0500

    start users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_031900) do
+ActiveRecord::Schema.define(version: 2021_01_05_113124) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user"
@@ -26,7 +26,18 @@ ActiveRecord::Schema.define(version: 2021_01_05_031900) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
