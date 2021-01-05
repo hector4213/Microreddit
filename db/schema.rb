221a0c61518eaa6382a3ac54996615ad2c3f8d7c
hector4213 <hectorpwns@gmail.com>
2021-01-05 07:19:13 -0500

    add fk to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_113124) do
+ActiveRecord::Schema.define(version: 2021_01_05_114505) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "user"
+    t.integer "user_id", null: false
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -39,5 +40,6 @@ ActiveRecord::Schema.define(version: 2021_01_05_113124) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
