1bc0b431ac816b7d6092ef852fa2e53d9475ea2d
hector4213 <hectorpwns@gmail.com>
2021-01-05 10:08:12 -0500

    remove relation do later

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
-    has_many :comments
-    belongs_to :user
     validates :title, presence: true, length: { minimum: 4 }
     validates :url, presence: true, length: { minimum: 6 }
 end
