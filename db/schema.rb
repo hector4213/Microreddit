520769f67e9a0822d4c493350c97b3206bf98492
hector4213 <hectorpwns@gmail.com>
2021-01-06 20:48:32 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_203024) do
+ActiveRecord::Schema.define(version: 2021_01_06_193310) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_01_05_203024) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_01_05_203024) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
