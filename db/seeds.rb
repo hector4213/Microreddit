ed2dd1e9de7d945018a8276f87b33d7bedfd8449
hector4213 <hectorpwns@gmail.com>
2021-01-07 14:51:20 -0500

    add posts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{ title: 'Some title', url: 'www.google.com', user_id: 1 }, { title: 'Another one', url: 'www.wedabest.com/we', user_id: 1 },
+                     { title: 'A new hope', url: 'www.starwars.com/galaxy/destroy', user_id: 1 }, { title: 'Rails is cool', url: 'www.rubyorg.com/gems', user_id: 1 }, { title: 'Some title again', url: 'www.bing.com', user_id: 1 }, { title: 'I just cant stop typing', url: 'www.myfingershurt.com', user_id: 1 }])
