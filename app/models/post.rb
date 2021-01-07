52fa1da08a3bbc45bae66899fd9a10c91ccdf76c
hector4213 <hectorpwns@gmail.com>
2021-01-06 20:48:02 -0500

    add validations for models, also add relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-    has_many :comments
-    validates :title, presence: true, length: { minimum: 4 }
-    validates :url, presence: true, length: { minimum: 6 }
+  belongs_to :user
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :url, presence: true, length: { minimum: 6 }
 end
