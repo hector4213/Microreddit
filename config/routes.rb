994701293915cd203a57f405fcd1fc4298aa9aff
hector4213 <hectorpwns@gmail.com>
2021-01-06 20:55:23 -0500

    do signup and add routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-
-resources :users
-resources :sessions
-  root "posts#index"
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  resources :users
+  root 'posts#index'
   resources :posts do
     resources :comments
   end
