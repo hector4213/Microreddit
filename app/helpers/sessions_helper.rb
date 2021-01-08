bb0ddc05c49d328143f044111fffc6e1efd69d81
hector4213 <hectorpwns@gmail.com>
2021-01-08 14:38:18 -0500

    remove helper for redirecting

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,15 +22,6 @@ module SessionsHelper
     user == current_user
   end
 
-  def redirect_back_or(default)
-    redirect_to(session[:forwarding_url] || default)
-    session.delete(:forwarding_url)
-  end
-
-  def store_location
-    session[:forwarding_url] = request.original_url if request.get?
-  end
-
 
   private
   def logged_in_user
