e50c19cde602e11bda5a9eb9f87b8e789bafc78a
hector4213 <hectorpwns@gmail.com>
2021-01-08 14:38:47 -0500

    add post helper for calculating points for post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+ #self is like this in js, wew
+
+ def up_votes
+  self.votes.where(vote: true).size
+end
+
+def down_votes
+  self.votes.where(vote: false).size
+end
+
+def vote_points
+  up_votes - down_votes
+end
+
 end
