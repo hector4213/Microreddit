871484865b7ee53c3550f82df70c1b0b881b495a
hector4213 <hectorpwns@gmail.com>
2021-01-08 13:46:28 -0500

    add member route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/signup', to: 'users#new'
+
   resources :users
-  root 'posts#index'
+
+# https://guides.rubyonrails.org/routing.html#adding-member-routes
+# This is a bit weird
+
   resources :posts do
+    member do
+      post :vote
+    end
     resources :comments
   end
 end
