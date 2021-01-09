fe69948b3c342cc9bbb7c2655963660e8059242a
hector4213 <hectorpwns@gmail.com>
2021-01-08 21:23:29 -0500

    helpers is now concern for controller

---
+++ app/controllers/concerns/voting.rb
@@ -0,0 +1,19 @@
+module PostsHelper
+  extend ActiveSupport::Concern
+  #self is like this in js, wew
+ 
+  def vote_points
+   up_votes - down_votes
+  end
+ 
+  def up_votes
+   self.votes.where(vote: true).size
+ end
+ 
+ def down_votes
+   self.votes.where(vote: false).size
+ end
+ 
+ 
+ end
+ 
