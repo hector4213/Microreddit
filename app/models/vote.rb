f62530f54bf1f3c38da733f67bffcb7960146227
hector4213 <hectorpwns@gmail.com>
2021-01-09 07:05:40 -0500

    ensure row is unique

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  #https://stackoverflow.com/questions/923796/how-do-you-validate-uniqueness-of-a-pair-of-ids-in-ruby-on-rails
+  #ensures both user_id and post_id is unique so user can only vote once.
+  validates :user_id, uniqueness: { scope: :post_id }
 end
