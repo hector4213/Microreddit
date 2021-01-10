ea69c32af95acbe0c3faec8dc783b2635e537774
hector4213 <hectorpwns@gmail.com>
2021-01-10 10:23:42 -0500

    add setter git post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user
+  before_action :set_post, only: [:show, :vote]
 
   def index
     @posts = Post.all
@@ -49,6 +50,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :url, :user_id)
+  end
+
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 
