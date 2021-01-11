61928577b4962bbf0d1957f58a5a9585184478aa
hector4213 <hectorpwns@gmail.com>
2021-01-11 10:21:50 -0500

    remove has many votes now voteable poly

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   include Voting
   belongs_to :user
   has_many :comments
-  has_many :votes
   validates :title, presence: true, length: { minimum: 4 }
   validates :url, presence: true, length: { minimum: 6 }
 end
