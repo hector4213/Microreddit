90bdef612910cd77d6b31076dc8384d3a7103feb
hector4213 <hectorpwns@gmail.com>
2021-01-11 00:10:11 -0500

    set user for routes except new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show]
-  before_action :set_user
+  before_action :set_user, except: [:new, :create]
 
   def show
     @user = User.find(params[:id])
