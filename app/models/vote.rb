590882fde5e3fb913edd10b07cefc24832b618a4
hector4213 <hectorpwns@gmail.com>
2021-01-11 00:12:32 -0500

    set vote to :voteable entity make association polymorphic

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,9 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  belongs_to :voteable, polymorphic: true
 
   #https://stackoverflow.com/questions/923796/how-do-you-validate-uniqueness-of-a-pair-of-ids-in-ruby-on-rails
   #ensures both user_id and post_id is unique so user can only vote once.
-  validates :user_id, uniqueness: { scope: :post_id }
+  validates_uniqueness_of :user_id, scope: [:voteable_id, :voteable_type]
 end
