8b1eb789c33dd670c130b94f5209eae76ac4bbda
hector4213 <hectorpwns@gmail.com>
2021-01-11 00:10:54 -0500

    add polymorphic relationship to comment and post

--- app/controllers/concerns/voting.rb
+++ app/controllers/concerns/voting.rb
@@ -3,6 +3,11 @@ module Voting
   #concerns are for DRYing code, used in controllers as methods, helpers are for views.
   #self is like this in js, wew
  
+  #now both votes and comments have votables relationships (polymorphic)
+  included do
+    has_many :votes, as: :voteable
+  end
+
   def vote_points
    up_votes - down_votes
   end
