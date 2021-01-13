136b0441832f70ea0cf50a85026ca6476d185eb0
hector4213 <hectorpwns@gmail.com>
2021-01-13 14:40:10 -0500

    remove before_action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :vote]
 
     def create
         @post = Post.find(params[:post_id])
@@ -31,7 +30,4 @@ class CommentsController < ApplicationController
         params.require(:comment).permit(:user, :body, :user_id)
     end
 
-    def set_post
-        @post = Post.find(params[:id])
-    end
 end
