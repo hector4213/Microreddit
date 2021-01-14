ecb49595b6205bae21166b6afa77053695ab2f7d
hector4213 <hectorpwns@gmail.com>
2021-01-14 06:48:04 -0500

    limit posts for pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,19 @@ class PostsController < ApplicationController
   before_action :logged_in_user
   before_action :set_post, only: [:show, :vote]
 
+  PER_PAGE = 10
+
   def index
     if params[:sort] == "recent"
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).limit(PER_PAGE)
     elsif
       params[:sort] == "top"
-      @posts = Post.all.sort { |a, b| b.vote_points <=> a.vote_points  }
+      @posts = Post.limit(PER_PAGE).sort { |a, b| b.vote_points <=> a.vote_points  }
     elsif 
       params[:sort] == "hot"
-      @posts = Post.all.order(created_at: :desc).sort { |a, b| b.vote_points <=> a.vote_points  }
+      @posts = Post.limit(PER_PAGE).order(created_at: :desc).sort { |a, b| b.vote_points <=> a.vote_points  }
     else
-      @posts = Post.all.order(created_at: :asc).sort { |a, b| b.vote_points <=> a.vote_points  }
+      @posts = Post.limit(PER_PAGE).order(created_at: :asc).sort { |a, b| b.vote_points <=> a.vote_points  }
     end
   end
 
