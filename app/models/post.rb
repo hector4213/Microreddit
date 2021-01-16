e2e466fb076f20ff0e22cb4090683f3ce62a1664
hector4213 <hectorpwns@gmail.com>
2021-01-16 10:35:01 -0500

    add dependency

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   include Voting
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes, as: :voteable
   validates :title, presence: true, length: { minimum: 4 }
   validates :url, presence: true, length: { minimum: 6 }
