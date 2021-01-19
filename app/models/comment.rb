20a2ad2144c4275f8a532fc75486285d8ed5b245
hector4213 <hectorpwns@gmail.com>
2021-01-19 14:28:51 -0500

    add relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_many :votes, as: :voteable
+  has_many :comment, :class_name => "Comment", :foreign_key => "parent_id"
+
+
 
   validates :body, presence: true
 end
