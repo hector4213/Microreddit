97bd049a7970965bbe65d153fbd17a51e9cdc499
hector4213 <hectorpwns@gmail.com>
2021-01-19 22:07:04 -0500

    add relationships(self-joins)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_many :votes, as: :voteable
-  has_many :comment, :class_name => "Comment", :foreign_key => "parent_id"
+
+  belongs_to :parent, class_name: "Comment", optional: true
+  has_many :replies, class_name: "Comment", :foreign_key => "parent_id"
 
   validates :body, presence: true
 end
